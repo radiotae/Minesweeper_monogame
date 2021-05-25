3f1c530cd96965f15f66db6891425e5653b6931c
radiotae <radiotae@gmail.com>
2021-05-25 09:29:29 +0900

    Add project files.

---
+++ Minesweeper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Minesweeper())
+                game.Run();
+        }
+    }
+}
